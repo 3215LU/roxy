1bac19cac721988a0e48341351d9b479ef094d63
Roxana <roxanayvbenicion.com>
2022-06-30 08:52:59 -0300

    :tp integrador JS terminado

---
+++ js/index.js
@@ -0,0 +1,24 @@
+ /*Es una función borrar, corresponde al boton Borrar*/
+function borrar(){
+    document.getElementById('totalCompra').innerHTML='Total a Pagar: $';
+    HTMLFormElement.reset()
+}
+ /*Es una función resume, corresponde al boton Resume*/
+function resume(){
+var cantidad= document.getElementById('cantidadEntrada').value;
+var categorias= document.getElementById('categoria').value;
+var total=Number;
+console.log(cantidad);
+    if(categorias=="estudiante"){
+      total=40*cantidad;//160 representa el 80%
+    }else{
+          if(categorias=="trainne"){
+          total=100*cantidad;//100 representa el 50%
+          }else{
+                if(categorias=="junior"){
+                total=170*cantidad;//100 representa el 15%
+                }
+          }
+    }
+  document.getElementById('totalCompra').innerHTML='Total a Pagar: $'+total;
+}
