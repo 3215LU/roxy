4fce1bf27f38a35eeabed8a462ced0ea14a3b6a1
Roxana <roxanayvbenicion.com>
2022-07-01 04:58:42 -0300

    modificado js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
- /*Es una función resume, corresponde al boton Resume*/
-function resume(event){
-//event.preventDefault();
-  /*
-HTMLFormElement.preventdefault();
-*/
-document.getElementById("botonCalcular").addEventListener("click", function(event){
-  event.preventDefault()
-});
 
-var cantidad= document.getElementById('cantidadEntrada').value;
-var categorias= document.getElementById('categoria').value;
-var total=Number;
-console.log(cantidad);
-    if(categorias=="estudiante"){
-      total=40*cantidad;//160 representa el 80%
-    }else{
-          if(categorias=="trainne"){
-          total=100*cantidad;//100 representa el 50%
-          }else{
-                if(categorias=="junior"){
-                total=170*cantidad;//100 representa el 15%
-                }
-          }
-    }
-  document.getElementById('totalCompra').innerHTML='Total a Pagar: $'+total;
+
  
-}
- /*Es una función borrar, corresponde al boton Borrar*/
- function borrar(){
-  document.getElementById('totalCompra').innerHTML='Total a Pagar: $';
-  HTMLFormElement.reset();
-}
